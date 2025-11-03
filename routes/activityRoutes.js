f0a25b9ecec33a93ef70a37cd22f1cbb4ba71dfa
Raj Kanase <rajkanase11@gmail.com>
2025-11-03 12:57:08 +0530

    Changed activities

--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User')
 const Activity = require('../models/Activity');
+const Model = require('../models/Model');
 
 // POST /api/activity/
 router.post('/', async (req, res) => {
@@ -10,25 +11,31 @@ router.post('/', async (req, res) => {
     const user = await User.findOne({ contact_number });
     if (user) {
       // Create a new activity (excluding contact_number)
-      
-      const activity = new Activity({
-        model,
-        defects,
-        final_price,
-      });
 
-      await activity.save();
+      // Load the model and populate all relevant defect arrays
+      const existingModel = await Model.findById(model)
 
-      // Push activity id into user's activities array (add this field to schema if not present)
-      if (!user.activities) user.activities = [];
-      user.activities.push(activity._id);
-      await user.save();
+      if (existingModel) {
 
-      res.json({
-        message: 'Activity created and user updated',
-        activityId: activity._id,
-        userId: user._id
-      });
+        const activity = new Activity({
+          model: existingModel._id,
+          defects,
+          final_price,
+        });
+
+        await activity.save();
+
+        // Push activity id into user's activities array (add this field to schema if not present)
+        if (!user.activities) user.activities = [];
+        user.activities.push(activity._id);
+        await user.save();
+
+        res.json({
+          message: 'Activity created and user updated',
+          activityId: activity._id,
+          userId: user._id
+        });
+      }
     } else {
       return res.status(400).json({ message: 'User not found' });
     }
