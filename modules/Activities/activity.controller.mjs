bb523824c6f9212e48bc3739bdc3034d0cf90489
codadhyay <codadhyay@gmail.com>
2026-04-03 15:01:36 +0530

    Handled status update

--- modules/Activities/activity.controller.mjs
+++ modules/Activities/activity.controller.mjs
@@ -160,7 +160,7 @@ export const updateActivityStatus = async (req, res) => {
         });
       }
 
-      activity.assigned_to = assigned_to;
+      activity.assigned_to = assignedUser._id;
     }
 
     activity.status = status;
